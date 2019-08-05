057c8b9589e18012aa01cefa2e7f6e5ed4beb5ac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-05 17:32:37 +0000

    Deploy saikiransripada/saikiransripada.github.io to github.com/saikiransripada/saikiransripada.github.io.git:master

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20190805170830";
+var CACHE_NAME = "saikiransripada-20190805173229";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?201908051708",
+        "/css/style.css?201908051732",
         "/"
       ]);
     })
